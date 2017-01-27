0dcd28628c6161b5ac413f5e3cc1523bce1b3e4b
Andre <dre.board@gmail.com>
2017-01-27 18:38:40 -0500

    Travisci verification icon in readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 ## Codeigniter 3 Dev Project
+
+[![Build Status](https://travis-ci.org/dreboard/ci3.svg?branch=master)](https://travis-ci.org/dreboard/ci3)
 A development project created with the Codeigniter framework to demonstrate basic routing and CRUD
 
 - Bootstrap 3.3.7 and smarty templating system.
--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,26 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends MY_Controller {
+
+	public function __construct() {
+		parent::__construct();
+	}
 
 	/**
-	 * Index Page for this controller.
 	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
+	public function index2()
+	{
+		$this->ci_smarty->assign("title", "Testing Smarty");
+		$this->ci_smarty->assign("description", $this->lang->line('site_smarty_description'));
+		$this->ci_smarty->assign("description", $this->lang->line('site_sample_data'));
+		$this->ci_smarty->view('index');
+
+	}
 	public function index()
 	{
-		$this->load->view('welcome_message');
+		$data['elapsed_time'] = 30;
+		$this->ci_smarty->view('welcome', $data);
 	}
 }
